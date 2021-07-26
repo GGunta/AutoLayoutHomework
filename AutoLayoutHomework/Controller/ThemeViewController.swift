dc192e3a9afa870dcff828958d3f313053a501a0
GGunta <msdoors@gmail.com>
2021-07-26 18:14:55 +0300

    error fixed

--- AutoLayoutHomework/Controller/ThemeViewController.swift
+++ AutoLayoutHomework/Controller/ThemeViewController.swift
@@ -18,10 +18,9 @@ class ThemeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        styleOutletsCollection.forEach { button in
-            button.layer.cornerRadius = 11
+        styleOutletsCollection.forEach { button in button.layer.cornerRadius = 10
             button.layer.borderColor = UIColor.darkGray.cgColor
-            button.layer.borderWidth = 3
+            button.layer.borderWidth = 2
         }
     }
  
@@ -33,34 +32,10 @@ class ThemeViewController: UIViewController {
     
     
     @IBAction func buttonDTapped(_ sender: Any) {
-        navigationItem.title = "Button D"
+        navigationItem.title = "Button C gone"
         self.buttonCOutlet.isHidden = true
         self.buttonEOutlet.setImage(UIImage(systemName: "sun"), for: .normal)
     }
     
 }
 
-/*
- class ThemeViewController: UIViewController {
-     @IBOutlet var styleOutletsCollection: [UIButton]!
-     @IBOutlet weak var buttonCOutlet: UIButton!
-     @IBOutlet weak var buttonEOutlet: UIButton!
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         styleOutletsCollection.forEach { button in
-             button.layer.cornerRadius = 11
-             button.layer.borderColor = UIColor.darkGray.cgColor
-             button.layer.borderWidth = 3
-         }
-     }
-     @IBAction func sunItemTapped(_ sender: Any) {
-         navigationItem.title = "Homework done!"
-         self.buttonCOutlet.isHidden = false
-     }
-     @IBAction func buttonDTapped(_ sender: Any) {
-         navigationItem.title = "Button D"
-         self.buttonCOutlet.isHidden = true
-         self.buttonEOutlet.setImage(UIImage(systemName: "sun"), for: .normal)
-     }
- }
- */
